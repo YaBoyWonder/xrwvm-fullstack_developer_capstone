6bb531c9e33b0eee6d1c8d151a918fee237bb451
ragu <ragu>
2024-09-16 17:35:37 -0400

    push

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,19 +1,22 @@
+# Uncomment the following imports before adding the Model code
+
 from django.db import models
 from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
 
     def __str__(self):
-        return self.name 
+        return self.name  # Return the name as the string representation
 
 
 class CarModel(models.Model):
     car_make = models.ForeignKey(
         CarMake, on_delete=models.CASCADE
-    )  # Many-to-One relationship
+    ) 
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ("SEDAN", "Sedan"),
